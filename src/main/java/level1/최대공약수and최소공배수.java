fc1f0c8145b05407d9d2a06f80ff655614a00de0
idohyeon <ehgus5026@naver.com>
2024-09-25 19:29:39 +0900

    -최대공약수, 최소공배수

---
+++ src/main/java/level1/최대공약수and최소공배수.java
@@ -0,0 +1,24 @@
+package level1;
+
+public class 최대공약수and최소공배수 {
+
+    class Solution {
+        public int[] solution(int n, int m) {
+            // 최소공배수 = n * m / 최대공약수
+            int[] answer = new int[2];
+            answer[0] = gcd(Math.max(n, m), Math.min(n, m)); // 12, 3 / 5, 2
+            answer[1] = (n * m) / answer[0];
+
+            return answer;
+        }
+
+        public int gcd(int max, int min) {
+            if(min == 0) {
+                return max;
+            }
+            return gcd(min, max % min);
+        }
+
+    }
+
+}
