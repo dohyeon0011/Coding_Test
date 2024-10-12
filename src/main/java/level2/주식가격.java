88295ef2f2d886c3580b5225d3844cd385b2a053
idohyeon <ehgus5026@naver.com>
2024-10-12 19:31:34 +0900

    -level2

---
+++ src/main/java/level2/주식가격.java
@@ -0,0 +1,26 @@
+package level2;
+
+public class 주식가격 {
+
+    class Solution {
+        public int[] solution(int[] prices) {
+            int[] answer = new int[prices.length];
+
+            for (int i = 0; i < prices.length; i++) {
+                int cnt = 0;
+
+                for (int j = i + 1; j < prices.length; j++) {
+                    cnt++;
+
+                    if (prices[i] > prices[j]) {
+                        break;
+                    }
+                }
+                answer[i] = cnt;
+            }
+
+            return answer;
+        }
+    }
+
+}
