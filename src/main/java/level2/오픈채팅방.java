e7f8f24ef5de8b4e5b187942c2129544ff9b7277
idohyeon <ehgus5026@naver.com>
2024-10-19 20:13:48 +0900

    -level2

---
+++ src/main/java/level2/오픈채팅방.java
@@ -0,0 +1,45 @@
+package level2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 오픈채팅방 {
+
+    class Solution {
+        public String[] solution(String[] record) {
+            Map<String, String> idNickMap = new HashMap<>();
+            int cnt = 0; // change일 때
+
+            // 먼저 최종적으로 닉네임이 어떻게 바꼈는 지 맵에 저장하기
+            for (int i = 0; i < record.length; i++) {
+                String[] info = record[i].split(" ");
+
+                if (info[0].equals("Leave")) {
+                    continue;
+                } else if (info[0].equals("Enter")) {
+                    idNickMap.put(info[1], info[2]);
+                } else {
+                    idNickMap.put(info[1], info[2]);
+                    cnt++;
+                }
+            }
+
+            String[] answer = new String[record.length - cnt];
+            int idx = 0;
+
+            for (int i = 0; i < record.length; i++) {
+                String[] info = record[i].split(" ");
+                String nickname = idNickMap.get(info[1]);
+
+                if (info[0].equals("Enter")) {
+                    answer[idx++] = nickname + "님이 들어왔습니다.";
+                } else if (info[0].equals("Leave")) {
+                    answer[idx++] = nickname + "님이 나갔습니다.";
+                }
+            }
+
+            return answer;
+        }
+    }
+
+}
---
+++ src/main/java/level2/택배상자.java
@@ -0,0 +1,34 @@
+package level2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class 택배상자 {
+
+    static int[] order = new int[]{4, 3, 1, 2, 5};
+
+    public static void main(String[] args) {
+        int answer = 0;
+
+        Stack<Integer> st = new Stack<>();
+        int size = order.length;
+        int idx = 0;
+
+        for (int i = 1; i <= size; i++) {
+            if (order[idx] != i) {
+                st.push(i);
+            } else {
+                idx++;
+                answer++;
+            }
+
+            while (!st.isEmpty() && st.peek() == order[idx]) {
+                st.pop();
+                idx++;
+                answer++;
+            }
+        }
+
+    }
+}
