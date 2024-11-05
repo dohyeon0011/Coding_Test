9bd9a754029f8f0e45395045c02fe79500fa2a80
idohyeon <ehgus5026@naver.com>
2024-11-05 19:22:17 +0900

    -baekjoon

---
+++ src/main/java/baekjoon/설탕배달.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+public class 설탕배달 {
+    static int sugarBox = 6;
+
+    public static void main(String[] args) {
+        // 5 5 5 3
+        int answer = 0;
+
+        while (sugarBox > 0) {
+            if (sugarBox % 5 == 0) {
+                answer += (sugarBox / 5);
+                break;
+            }
+
+            sugarBox -= 3;
+            answer++;
+
+            if (sugarBox < 0) {
+                answer = -1;
+                break;
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+}
