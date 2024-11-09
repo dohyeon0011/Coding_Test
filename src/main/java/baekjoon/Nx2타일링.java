f1629811973847437b4453a7d44ef6acefc8a097
idohyeon <ehgus5026@naver.com>
2024-11-09 17:30:48 +0900

    -dp

---
+++ src/main/java/baekjoon/Nx2타일링.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Nx2타일링 {
+
+    public static void main(String[] args) throws IOException {
+        // 1 1
+        // 2 2
+        // 3 3
+        // dp[0]은 0이지만 1로 해두고 해야됨. dp[2]까지 선언해버리면 n이 1이 들어왔을 때,
+        // 배열 크기는 2인데 dp[2]까지 선언이 되면 런타임 에러 (ArrayIndexOutOfBounds)이 뜨기 때문.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int len = Integer.parseInt(st.nextToken());
+        int[] dp = new int[len + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= len; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+
+        System.out.println(dp[len]);
+    }
+}
---
+++ src/main/java/baekjoon/피보나치함수.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class 피보나치함수 {
+    static int[] cnt0Arr;
+    static int[] cnt1Arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int len = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < len; i++) {
+            int num = Integer.parseInt(br.readLine());
+            getFibo(num);
+
+            System.out.println(cnt0Arr[num] + " " + cnt1Arr[num]);
+        }
+    }
+
+    private static void getFibo(int num) {
+        cnt0Arr = new int[41];
+        cnt1Arr = new int[41];
+        cnt0Arr[0] = 1;
+        cnt1Arr[0] = 0;
+        cnt0Arr[1] = 0;
+        cnt1Arr[1] = 1;
+
+        for (int j = 2; j <= num; j++) {
+            cnt0Arr[j] = cnt0Arr[j - 1] + cnt0Arr[j - 2];
+            cnt1Arr[j] = cnt1Arr[j - 1] + cnt1Arr[j - 2];
+        }
+    }
+
+}
