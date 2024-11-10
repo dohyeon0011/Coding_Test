5296b2ec33f5948f31201c3217edbd166b8f456c
idohyeon <ehgus5026@naver.com>
2024-11-10 15:41:56 +0900

    -dp

---
+++ src/main/java/baekjoon/DP/계단오르기.java
@@ -0,0 +1,33 @@
+package baekjoon.DP;
+
+import java.io.*;
+
+public class 계단오르기 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] stairs = new int[num + 1]; // 해당 계단의 점수
+        int[] maxScore = new int[num + 1]; // 해당 계단에서의 최고 점수를 저장할 곳
+
+        for (int i = 1; i <= num; i++) {
+            stairs[i] = Integer.parseInt(br.readLine());
+        }
+
+        maxScore[1] = stairs[1];
+        maxScore[2] = stairs[1] + stairs[2];
+        maxScore[3] = Math.max(stairs[1], stairs[2]) + stairs[3];
+
+        for (int i = 4; i <= num; i++) {
+            maxScore[i] = Math.max(maxScore[i - 3] + stairs[i - 1], maxScore[i - 2]) + stairs[i];
+        }
+
+        bw.write(String.valueOf(maxScore[num]));
+
+        br.close();
+        bw.close();
+    }
+
+}
---
+++ src/main/java/baekjoon/DP/부녀회장이될테야.java
@@ -0,0 +1,42 @@
+package baekjoon.DP;
+
+import java.io.*;
+
+public class 부녀회장이될테야 {
+
+    public static void main(String[] args) throws IOException {
+        // 2 0 1 4 10 20
+        // 1 0 1 3 6 10
+        // 0 0 1 2 3 4
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < num; i++) {
+            int k = Integer.parseInt(br.readLine()); // 층
+            int n = Integer.parseInt(br.readLine()); // 호
+            int[][] dp = new int[k + 1][n + 1];
+
+            for (int j = 1; j <= n; j++) {
+                dp[0][j] = j;
+            }
+
+            extracted(k, n, dp);
+
+            bw.write(String.valueOf(dp[k][n]) + "\n");
+//            System.out.println(dp[k][n]);
+        }
+
+        br.close();
+        bw.close();
+    }
+
+    private static void extracted(int k, int n, int[][] dp) {
+        for (int j = 1; j <= k; j++) {
+            for (int h = 1; h <= n; h++) {
+                dp[j][h] = dp[j - 1][h] + dp[j][h - 1];
+            }
+        }
+    }
+
+}
