d79a5c2fb9be120e5cf52ad043ffc50c910d41aa
idohyeon <ehgus5026@naver.com>
2024-11-26 17:03:09 +0900

    -dp

---
+++ src/main/java/baekjoon/DP/거스름돈_14916.java
@@ -0,0 +1,32 @@
+package baekjoon.DP;
+
+import java.io.*;
+
+public class 거스름돈_14916 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int money = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        while (money > 0) {
+            if (money % 5 == 0) {
+                answer += (money / 5);
+                break;
+            }
+            money -= 2;
+            answer++;
+
+            if (money < 0) {
+                answer = -1;
+                break;
+            }
+        }
+
+        bw.write(String.valueOf(answer));
+
+        br.close();
+        bw.close();
+    }
+}
---
+++ src/main/java/baekjoon/DP/돌게임2_9656.java
@@ -0,0 +1,38 @@
+package baekjoon.DP;
+
+import java.io.*;
+
+public class 돌게임2_9656 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[1001];
+
+        // 홀수 : 창영(CY)승, 짝수 : 상근(SK)승
+        // 4:2
+        // 5:3
+        // 6:2
+        // 7:3
+        // 8:4
+        // 2 1 2
+        // 3 2 3
+        // 4 3 4
+        dp[1] = 2;
+        dp[2] = 1;
+        dp[3] = 2;
+        for (int i = 4; i <= num; i++) {
+            dp[i] = Math.min(dp[i - 1], dp[i - 3]) + 1;
+        }
+
+        if (dp[num] % 2 == 0) {
+            bw.write("CY");
+        } else {
+            bw.write("SK");
+        }
+
+        br.close();
+        bw.close();
+    }
+}
