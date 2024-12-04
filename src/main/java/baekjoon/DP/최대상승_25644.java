609def24da343f2b3f96b93eeece34721370d83a
idohyeon <ehgus5026@naver.com>
2024-12-04 15:01:22 +0900

    -dp

--- src/main/java/baekjoon/DP/최대상승_25644.java
+++ src/main/java/baekjoon/DP/최대상승_25644.java
@@ -1,7 +1,30 @@
 package baekjoon.DP;
 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class 최대상승_25644 {
-    public static void main(String[] args) {
-        System.out.println("gd");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int day = Integer.parseInt(br.readLine());
+
+        int min = Integer.MAX_VALUE; // 그전까지의 최소값
+        int res = 0; // 그전까지의 최대값
+
+        st  = new StringTokenizer(br.readLine());
+        for (int i = 0; i < day; i++) {
+            int stockPrice = Integer.parseInt(st.nextToken()); // 현재 시점
+
+            res = Math.max(res, stockPrice - min); // 현 시점기준 최대값 비교(그 전까지의 res(최대값), 현재 값 - 그 전까지의 최소값)
+            min = Math.min(min, stockPrice);  // 현 시점기준 최소값 비교(그 전까지의 min(최소값), 현재값)
+        }
+
+        bw.write(String.valueOf(res));
+
+        br.close();
+        bw.close();
     }
 }
