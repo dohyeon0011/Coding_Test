2a787f0d045f44da77ef633f52fa8e02a28ec03c
idohyeon <ehgus5026@naver.com>
2024-12-06 17:47:42 +0900

    -Int Array Sort

---
+++ src/main/java/baekjoon/수정렬하기2_2751.java
@@ -0,0 +1,28 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 수정렬하기2_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int len = Integer.parseInt(br.readLine());
+        int[] intArr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            intArr[i] = num;
+        }
+        Arrays.sort(intArr);
+
+        for (int n : intArr) {
+            bw.write(String.valueOf(n) + "\n");
+        }
+        br.close();
+        bw.close();
+    }
+}
