d3b790a7a0a89fa96ea1c6d078160b900962d64d
idohyeon <ehgus5026@naver.com>
2024-12-10 15:44:13 +0900

    -math

---
+++ src/main/java/baekjoon/분수찾기_1193.java
@@ -0,0 +1,35 @@
+package baekjoon;
+
+import java.io.*;
+
+public class 분수찾기_1193 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int num = Integer.parseInt(br.readLine());
+        int floor = 1; // 층수
+
+        while (num - floor > 0) { // 몇 층인지
+            num -= floor;
+            floor++;
+        }
+
+        int idx = num; // 몇 번째 칸인지
+        int sum = floor + 1; // 분모와 분자의 합
+        int a = 0, b = 0; // 분모, 분자
+
+        if (floor % 2 == 1) {
+            a = idx;
+            b = sum - idx;
+        } else {
+            a = sum - idx;
+            b = idx;
+        }
+
+        bw.write(String.valueOf(b) + "/" + String.valueOf(a));
+
+        br.close();
+        bw.close();
+    }
+}
